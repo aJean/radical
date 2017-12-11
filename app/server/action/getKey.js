da96eda3ce400eb9b331af1eda96ebf751049b7d
qiaoyue <qiaoyue@baidu.com>
2017-12-11 19:37:34 +0800

    update

---
+++ app/server/action/getKey.js
@@ -0,0 +1,10 @@
+/**
+ * @file 获取密钥
+ */
+
+module.exports = function (req, res) {
+    res.json({
+        errno: 0,
+        key: 'baiduid'
+    });
+};
--- tools/packer.js
+++ tools/packer.js
@@ -7,11 +7,6 @@ const crypto = require('crypto');
  */
 
 let dirPath = process.argv.splice(2)[0];
-let isstart = true;
-
-if (!dirPath) {
-    errorLog('no input files');
-}
 
 function errorLog(msg) {
     console.log('\x1B[31m', `error: ${msg}`);
@@ -24,23 +19,28 @@ function normalizePath(filePath) {
     return `${path.resolve('./')}/${filePath}`;
 }
 
+// encode to base64
 function convertFile(filepath) {
     return new Promise((resolve, reject) => {
         fs.readFile(filepath, (err, data) => {
-            err ? reject(err)
-                : resolve('data:image/jpg;base64,' + data.toString('base64') + '~#~');
+            if (err) {
+                reject(err);
+            } else {
+                data = 'data:image/jpg;base64,' + data.toString('base64');
+                const cipher = crypto.createCipher('aes-256-cbc', 'baiduid');
+                data = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+
+                resolve(data + '~#~');
+            }
         });
     });
 }
 
-// 保持顺序合并加密
-function encodeOrderFile() {
-    Promise.all(flags.map(name => convertFile(dirPath + '/mobile_' + name + '.jpg')))
+// 保持顺序合并
+function encodeFile(flags) {
+    Promise.all(flags.map(name => convertFile(dirPath + name)))
         .then(ret => {
-            let data = ret.join('');
-            const cipher = crypto.createCipher('aes-256-cbc', 'baiduid');
-            data = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
-
+            const data = ret.join('');
             fs.writeFile(dirPath + '/images.bxl', data, err => {
                 if (err) {
                     throw new Error(err);
@@ -49,26 +49,14 @@ function encodeOrderFile() {
         }).catch(e => errorLog(e));
 }
 
-// 分别加密
-function encodeFile(filename) {
-    flags.forEach(str => {
-        const filename = '/mobile_' + str;
-        const filepath = dirPath + filename + '.jpg';
+if (!dirPath) {
+    errorLog('no input files');
+} else {
+    const dh = crypto.createDiffieHellman(128);
+    const key = dh.generateKeys('hex');
+    console.log(dh.computeSecret(key, 'hex', 'base64'));
+    console.log(dh.computeSecret(key, 'hex', 'base64'));
 
-        fs.readFile(filepath, (err, data) => {
-            const encodePath = `${dirPath}/${filename}.bxl`;
-            const encodeData = 'data:image/jpg;base64,' + data.toString('base64');
-    
-            !err ? fs.writeFile(encodePath, encodeData, err => {
-                if (err) {
-                    throw new Error(err);
-                }
-            }) : errorLog(err);
-        });
-    });
+    dirPath = normalizePath(dirPath);
+    encodeFile(['/mobile_r.jpg', '/mobile_l.jpg', '/mobile_u.jpg', '/mobile_d.jpg', '/mobile_f.jpg', '/mobile_b.jpg']);
 }
-
-dirPath = normalizePath(dirPath);
-
-const flags = ['r', 'l', 'u', 'd', 'f', 'b'];
-encodeOrderFile();
