7f168edb73639df7b5b5f1f0a5ec2195c1962ece
qiaoyue <qiaoyue@baidu.com>
2018-05-22 14:02:52 +0800

    update

--- dist/lib.js
+++ dist/lib.js
@@ -53403,6 +53403,16 @@ var composeKey = function (part) { return ('skt1wins' + part); };
             }
         });
         return obj;
+    },
+    testWebgl: function () {
+        try {
+            var canvas = document.createElement('canvas');
+            return !!(window['WebGLRenderingContext'] && (canvas.getContext('webgl') ||
+                canvas.getContext('experimental-webgl')));
+        }
+        catch (e) {
+            return false;
+        }
     }
 });
 
--- src/core/util.ts
+++ src/core/util.ts
@@ -246,5 +246,17 @@ export default {
         });
 
         return obj;
-    }
+    },
+
+    testWebgl() {
+		try {
+			var canvas = document.createElement('canvas');
+			return !!( window['WebGLRenderingContext'] && (
+				canvas.getContext('webgl') ||
+				canvas.getContext('experimental-webgl'))
+			);
+		} catch ( e ) {
+			return false;
+		}
+	}
 };
