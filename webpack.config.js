f3ca710e92af1b7177e18cd430508b31e1935836
qiaoyue <qiaoyue@baidu.com>
2018-07-27 12:17:07 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     mode: 'development',
     devtool: 'none',
     optimization: {
-        minimize: false
+        minimize: true
     },
     entry: {
         lib: './src/index.ts'
